9794d3860d02631bbd76fd9d9a70004a56f86260
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2023-02-23 01:01:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['919148942515'] //ur owner number
+global.ownername = "ViV3K" //ur owner name
+global.ytname = "YT: ." //ur yt chanel name
+global.socialm = "GitHub: troublemaker515" //ur github or insta name
+global.location = "India, Bangalore" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
-global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
+global.ownernomer = "919148942515" //ur number
+global.premium = ['919148942515'] //ur premium number
+global.botname = 'TroubleMaker' //ur bot name
+global.linkz = "https://chat.whatsapp.com/GvgzPtKdvNd6HJVhnDVjIT" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/GvgzPtKdvNd6HJVhnDVjIT" //ur website to be displayed
+global.botscript = 'https://github.com/troublemaker515' //script link
 global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.packname = "Troublemaker" //ur sticker watermark packname
+global.author = "Viv3k" //ur sticker watermark author
+global.wm = "Troublemaker" //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -48,7 +48,7 @@ global.mess = {
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature could be used by bot only',
     wait: 'Please with...',
-    wal: 'ටිකක් හිටහන් බඩු මග එන ගමන්',
+    wal: '..............',
     linkm: 'Where is the link?',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
